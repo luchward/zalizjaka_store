ebb5ea5582e52da191475863e9763d197f55f3b1
Edward <50621180+edglock@users.noreply.github.com>
2019-10-13 17:24:53 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,33 @@
-document.getElementById('ref')
+/*document.getElementById('ref')
     .addEventListener('click', addSection);
 
-function addSection () {
+function addSection () {*/
     const sect = document.getElementById('extra');
-    const hat = document.createElement('h3');
+    let title = document.createElement('h3');
+    let headline = document.createElement('h4');
+    let para = document.createElement('p');
+    let span = document.createElement('span');
+    let form = document.createElement('form');
+    let input = document.createElement('input');
+    let select = document.createElement('select');
+    let option = document.createElement('option');
+    let type;
     hat.textContent = 'WOW! You have found extra capabilities of the site!';
     sect.appendChild(hat);
     sect.setAttribute('class', 'highlight');
-}
+    
+    const currencyConverter = document.createElement('div');
+    currencyConverter.appendChild('h4');
+    
+//}
+/*<p>I want to exchange</p>
+<input type="number" name="quantity" placeholder="0.00">
+<select name="currencyFrom">
+    <option value="usd">USD</option>
+</select>
+<p>to</p>
+<select name="currencyTo">
+        <option value="usd">USD</option>
+</select>
+<p>at rate from <a href="">SITE EXCHANGE</a>
+<p>Your result is <span>(result)</span></p>*/
