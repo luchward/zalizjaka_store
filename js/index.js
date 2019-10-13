fceed0f9f160b406532fafa55773c88bdc57395f
Edward <50621180+edglock@users.noreply.github.com>
2019-10-13 13:10:45 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,5 +2,8 @@ document.querySelector('header nav h3')
     .addEventListener('click', addSection);
 
 function addSection () {
-    
+    const sect = document.createElement('div');
+    const hat = document.createElement('h3');
+    hat.textContent = 'WOW! You have found extra capabilities of the site!';
+    sect.appendChild(hat);
 }
