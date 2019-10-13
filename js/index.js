45fef8f77fb533442b9a4ca884f8a8a6bb51e10e
Edward <edglock@gmail.com>
2019-10-13 11:08:32 +0000

    extra section developing

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
-document.querySelector('header nav h3')
+document.getElementById('ref')
     .addEventListener('click', addSection);
 
 function addSection () {
-    const sect = document.createElement('div');
+    const sect = document.getElementById('extra');
     const hat = document.createElement('h3');
     hat.textContent = 'WOW! You have found extra capabilities of the site!';
     sect.appendChild(hat);
+    sect.setAttribute('class', 'highlight');
 }
