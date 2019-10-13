2b78a906222bf7992e0131d0e8d72adff794ca4b
Edward <50621180+edglock@users.noreply.github.com>
2019-10-13 17:25:38 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 /*document.getElementById('ref')
     .addEventListener('click', addSection);
 
-function addSection () {*/
+function addSection () {
     const sect = document.getElementById('extra');
     let title = document.createElement('h3');
     let headline = document.createElement('h4');
@@ -19,15 +19,4 @@ function addSection () {*/
     const currencyConverter = document.createElement('div');
     currencyConverter.appendChild('h4');
     
-//}
-/*<p>I want to exchange</p>
-<input type="number" name="quantity" placeholder="0.00">
-<select name="currencyFrom">
-    <option value="usd">USD</option>
-</select>
-<p>to</p>
-<select name="currencyTo">
-        <option value="usd">USD</option>
-</select>
-<p>at rate from <a href="">SITE EXCHANGE</a>
-<p>Your result is <span>(result)</span></p>*/
+}*/
